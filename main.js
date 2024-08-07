96065172d9be870bbf705831483cf298b5c7f73c
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-08-07 12:24:46 -0500

    setup a projects array with one dummy object

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("connected")
+const projects = [
+  {
+  id: 1,
+  title: "dummyTitle",
+  description: "Great dummy project. Real good. good stuff here",
+  updatedTimeSig: "Updated 1,200 years ago",
+  vis: true,
+  },
+]
