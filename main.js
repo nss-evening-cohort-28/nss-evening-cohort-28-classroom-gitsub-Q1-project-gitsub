7943dbe09e5c848f009e71dd047d208b64b93f05
MaxJones129 <maxjones129@gmail.com>
2024-08-06 21:23:53 -0500

    importing arrays

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { pinnedRepos } from "../pinnedRepos.js";
+import { repos } from "../reposArray.js";
 
 const renderToDom = (divId, htmlToRender) => {
   const selectedDiv = document.querySelector(divId);
