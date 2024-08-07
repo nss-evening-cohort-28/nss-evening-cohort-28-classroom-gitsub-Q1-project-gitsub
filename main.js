b60dbcd9bb3ab58dab3f408d16f348791e715f41
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-08-07 13:01:05 -0500

    create projectsOnDom function

--- main.js
+++ main.js
@@ -7,3 +7,40 @@ const projects = [
   vis: true,
   },
 ]
+
+// projectCount to show how many projects "open" in header
+const projectCount = projects.length;
+
+const projectsOnDom = () => {
+  let domString = `
+    <div class="card" style="width: 18rem;">
+      <div class="card-header">
+        ${projectCount} Open
+      </div>
+      <ul class="list-group list-group-flush">
+  `;
+  for (project of projects) {
+    domString += `
+        <li class="list-group-item">
+          <div>
+            ${projects.title}
+            ${projects.vis}
+            ${projects.updatedTimeSig}
+          </div>
+          <div>
+            ${projects.description}
+          </div>
+        </li>
+    `
+  }
+  domString += `
+        </ul>
+    </div>
+  `
+  renderToDom(projects, domString);
+}
+
+// "domElement would be where you pass in your dom string variable"
+const renderToDom = (array, html) => {
+  domElement.i
+}
