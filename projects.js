41acf71f1da416c8a8d015d0aa217ceafbdf903b
MaxJones129 <maxjones129@gmail.com>
2024-08-06 21:29:18 -0500

    deleted useless array

---
+++ projects.js
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Example 1",
+    description: "No description"
+  },
+  {
+    id: 2,
+    title: "my-goals",
+    description: "No description"
+  },
+  {
+    id: 3,
+    title: "Sample My Goals",
+    description: "Goals For The NSS Bootcamp"
+  }
+]
