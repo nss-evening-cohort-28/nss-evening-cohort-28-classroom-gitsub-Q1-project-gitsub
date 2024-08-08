fd473e1c64796a40dadc758eb4dfd1995d063c05
Casey <dinnerdoggy@gmail.com>
2024-08-07 20:49:37 -0500

    set up creation form, need to flesh out bugs

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const projects = [
 // projectCount to show how many projects "open" in header
 const projectCount = projects.length;
 
+// Renders the project cards onto the dom from the projects array
 const projectsOnDom = () => {
   let domString = `
     <div class="card" style="width: 18rem;">
@@ -29,12 +30,16 @@ const projectsOnDom = () => {
   for (project of projects) {
     domString += `
         <li class="list-group-item">
-          <div>
+          <div id="titleEl">
             ${project.title}
-            ${project.vis}
-            ${project.updatedTimeSig}
           </div>
-          <div>
+          <div id="visEl">
+            ${project.vis}
+          </div>
+          <div id="timeSigEl">
+          ${project.updatedTimeSig}
+          </div>
+          <div id="descEl">
             ${project.description}
           </div>
         </li>
@@ -49,10 +54,35 @@ const projectsOnDom = () => {
 
 
 
-// "domElement would be where you pass in your dom string variable"
+// Renders arguments to the dom. divID is the target div you want to inject the html. "html" would be your "domString"
 const renderToDom = (divID, html) => {
   const targetDiv = document.querySelector(divID);
   targetDiv.innerHTML = html;
+}
+
+
+// Create, the "C" in CRUD
+const createProject = (e) => {
+e.preventDefault(); // stops page from refreshing
+  const newProjectObject = {
+      id: projects.length + 1,
+      title: document.querySelector("#projectNameInput").value,
+      description: document.querySelector("#descInput").value,
+      updatedTimeSig: document.querySelector("#timeSigEl").value,
+      vis: document.querySelector("#privateCheck").checked
+      };
+
+  projects.push(newProjectObject);
+
+  domString.innerHTML = "";
+  renderDom("#projects", domString);
+  //nameFrm.reset();
+}
+
+const submitForm = document.querySelector("#submitBtn")
+
+submitForm.addEventListener("click", (e)) => {
+  if (e.target.id.includes())
 }
 
 projectsOnDom();
