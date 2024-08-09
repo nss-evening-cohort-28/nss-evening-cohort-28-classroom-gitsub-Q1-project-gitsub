1fb2131b80734287dec587e7fb90cd3963ca6e38
Tion Blackmon <tion.blackmon@outlook.com>
2024-08-09 13:29:25 -0500

    The form to create a new repository is working. What typed in the form is rendering a new repo card to the DOM.

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 import { repositories } from "./data.js";
 import { repositoryCards } from "./components/repositoryCards.js";
 
-// Repository Page
+/*
+===================== Repository List =====================
+*/
+
+// Collect the form data.
+const createRepository = () => {
+  const newRepository = {
+    id: repositories.length + 1,
+    name: document.querySelector('#repo-name').value,
+    description: document.querySelector('#repo-description').value,
+    // stackTools: document.querySelector(''),
+    // repoStats: document.querySelector(''),
+  };
+
+  // Push object to the repositories array.
+  repositories.push(newRepository);
+
+  // Render the repos array.
+  repositoryCards(repositories);
+};
+
 repositoryCards(repositories);
+
+// Event listeners
+const repoForm = document.querySelector('#target-repo-form'); // Targeting the form.
+repoForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  createRepository();
+  repoForm.reset();
+});
