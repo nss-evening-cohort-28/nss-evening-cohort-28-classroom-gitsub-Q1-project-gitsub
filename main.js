51c66d92b5dcbb274692cbba0040cc7e153f9e03
MaxJones129 <maxjones129@gmail.com>
2024-08-08 20:56:01 -0500

    projects prebuild

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ const renderToDom = (divId, htmlToRender) => {
   const selectedDiv = document.querySelector(divId);
   selectedDiv.innerHTML = htmlToRender;
 };
+
+// for (const project of array) {
+//   <div class="card" style="width: 40rem;">
+//     <ul class="list-group list-group-flush">
+//       <div class = "projectsRow">
+//         <li class="list-group-item">${project.name}</li>
+//         <li class="list-group-item">${project.description}</li>
+//       </div>
+//     </ul>
+//   </div>
+// }
